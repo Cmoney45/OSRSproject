529d35a9bfc4a451a989771450d9e400d06da4bb
Cmoney <cascherer42@gmail.com>
2018-12-24 20:55:29 -0600

    initial commit

---
+++ client/src/pages/Home.js
@@ -0,0 +1,14 @@
+// client.src.pages.Home.js
+
+import React, { Component } from "react";
+
+class Home extends Component {
+
+  render() {
+    return (
+              <h1>Home</h1>
+    );
+  }
+}
+
+export default Home;
