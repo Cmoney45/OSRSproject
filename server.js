3adb4f7afd43e352c9dbf926508f430010630800
Cmoney <cascherer42@gmail.com>
2019-07-04 13:56:26 -0500

    Initial upload

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/MERN-user-framework",
+  process.env.MONGODB_URI || "mongodb://localhost/osrsProject",
   { useNewUrlParser: true }
 );
 
